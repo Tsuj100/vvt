1359a99542628ebacd8f7f0bde83c9b5d05aa875
Tsuj100 <racetop@outlook.com>
2022-02-18 17:48:03 +0800

    css引入为es模块

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Layout, Menu } from "ant-design-vue";
 
-import "ant-design-vue/lib/layout/style/index.css"
-import "ant-design-vue/lib/menu/style/index.css"
+import "ant-design-vue/es/layout/style/index.css"
+import "ant-design-vue/es/menu/style/index.css"
 
 createApp(App).use(Layout).use(Menu).mount("#app");
