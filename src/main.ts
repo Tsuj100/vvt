eb8d71840506dbb951dab391138932b0a3f02908
Tsuj100 <racetop@outlook.com>
2022-03-07 21:02:03 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Layout, Menu } from "ant-design-vue";
 
-import "ant-design-vue/es/layout/style/index.css";
-import "ant-design-vue/es/menu/style/index.css";
+import 'ant-design-vue/dist/antd.css';
 
 import { router } from "./router";
 
