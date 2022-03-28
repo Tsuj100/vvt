370423cfd10622c3e3af41f0db1a198c678f2e54
Tsuj100 <racetop@outlook.com>
2022-03-28 15:13:58 +0800

    修改开发或生产环境服务的公共基础路径为相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { ElementPlusResolver, AntDesignVueResolver } from "unplugin-vue-componen
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "./",
     plugins: [
         vue(),
         AutoImport({
