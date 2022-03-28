e951effcbdeb2c645d352861c9f8ee592266571f
Tsuj100 <racetop@outlook.com>
2022-03-28 13:49:46 +0800

    添加element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { Layout, Menu } from "ant-design-vue";
+import ElementPlus from "element-plus";
 
-import 'ant-design-vue/dist/antd.css';
+import "element-plus/dist/index.css";
+import "ant-design-vue/dist/antd.css";
 
 import { router } from "./router";
 
@@ -11,5 +13,6 @@ const app = createApp(App);
 app.use(Layout);
 app.use(Menu);
 app.use(router);
+app.use(ElementPlus);
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    css: {
+        postcss: {
+            plugins: [
+                {
+                    postcssPlugin: "internal:charset-removal",
+                    AtRule: {
+                        charset: (atRule) => {
+                            if (atRule.name === "charset") {
+                                atRule.remove();
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
 });
